00f165aea88b3cc3a96e2423b14f7b26cf3f62ed
keeganridgley107 <keeganwridgley@gmail.com>
2019-10-31 18:06:50 -0600

    import statement works, but convos are wonky needs logic work still feedback loop works db works, I think?

--- py_chatter_bot/app.py
+++ py_chatter_bot/app.py
@@ -10,16 +10,21 @@ from pathlib import Path
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
+from chatterbot.conversation import Statement
+import logging 
+import random
 
 nori = ChatBot('Nori',
-              logic_adapters=[
-                  'chatterbot.logic.MathematicalEvaluation',
-                  'chatterbot.logic.TimeLogicAdapter'
-              ],
-              storage_adapter='chatterbot.storage.SQLStorageAdapter')
+               storage_adapter='chatterbot.storage.SQLStorageAdapter',
+               logic_adapters=[
+                   'chatterbot.logic.MathematicalEvaluation',
+                   'chatterbot.logic.BestMatch'
+               ],
+               database_uri='sqlite:///database.db')
 list_trainer = ListTrainer(nori)
 corpus_trainer = ChatterBotCorpusTrainer(nori)
-
+logger = logging.getLogger() 
+logger.setLevel(logging.CRITICAL)
 
 def main():
     """
@@ -38,31 +43,34 @@ def create():
     function that trains the chatbot
 
     """
-    use_custom = input("\n\nUse custom corpus? y/n\n")
+
+    use_custom = input("\nUse custom corpus? y/n\n")
     if use_custom == "y":
         print("\nPlease type the list corpus filename below\n")
         print("\nMust be a list format txt file located in /assets\n")
         time.sleep(.4)
-        corpus_filename = input("Filename:>  ")
+        corpus_filename = input("Filename:>  ") + ".txt"
         data_folder = Path("assets/")
         file_to_open = data_folder / corpus_filename
         conv = open(file_to_open, 'r').readlines()
         # conv =  open('../assets/basic_1.txt', 'r').readlines()
         list_trainer.train(conv)
     else:
-        corpus_trainer.train("chatterbot.corpus.english")
+        corpus_trainer.train(
+            "chatterbot.corpus.english"
+            )
 
 
 def get_feedback():
 
     text = input()
 
-    if 'yes' in text.lower():
+    if 'y' in text.lower():
         return True
-    elif 'no' in text.lower():
+    elif 'n' in text.lower():
         return False
     else:
-        print('Please type either "Yes" or "No"')
+        print('Please type either "Y" or "N"')
         return get_feedback()
 
 
@@ -72,18 +80,46 @@ def chat():
 
     """
 
+    print("Begin chatting with Nori! ")
     print('\nPress ctrl-c or ctrl-d on the keyboard to exit\n')
     time.sleep(.5)
-
+    # The following loop will execute each time the user enters input
     while True:
         try:
-            user_input = input('You: ')
-            bot_response = nori.get_response(user_input)
-            print("Nori: {}".format(bot_response))
+            user_input = input("You: ")
+            input_statement = Statement(user_input)
+            response = nori.get_response(
+                input_statement
+            )
+
+            # get feedback every few questions
+            r_int = random.randint(0, 10)
+            print("rand num: {}".format(r_int))
+            if r_int < 9:
+                print("Nori: {}".format(response))
+            else:
+                print('\n Is "{}" a coherent response to "{}"? \n'.format(
+                    response.text,
+                    input_statement.text
+                ))
+                if get_feedback() is False:
+                    print('please input the correct one')
+                    correct_response = Statement(text=input())
+                    nori.learn_response(correct_response, input_statement)
+                    print('Responses added to bot!')
 
         # Press ctrl-c or ctrl-d on the keyboard to exit
         except (KeyboardInterrupt, EOFError, SystemExit):
             break
+    #while True:
+    #    try:
+    #        user_input = input('You: ')
+    #        bot_response = nori.get_response(user_input)
+    #        print("Nori: {}".format(bot_response))
+    #
+    #    # Press ctrl-c or ctrl-d on the keyboard to exit
+    #    except (KeyboardInterrupt, EOFError, SystemExit):
+    #        break
 
 
 if __name__ == '__main__':
